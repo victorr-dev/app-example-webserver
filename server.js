51224c1dc14458691175280afbc8f3142b218d2a
Victor Ruiz <vik_ases@live.com.m.x>
2019-11-29 17:34:16 -0600

    Listo para producción

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+const hbs = require('hbs')
+
+require('./hbs/helpers')
+
+const PORT = process.env.PORT | 3000
+
+app.use(express.static(__dirname + '/public'))
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs')
+
+//helpers
+
+
+
+app.get('/', (req, res) => {
+    res.render('home', {
+        nombre: 'victor'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about')
+})
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`)
+})
+
